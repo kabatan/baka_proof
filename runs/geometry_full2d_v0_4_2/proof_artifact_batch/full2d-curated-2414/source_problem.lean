fef8e11e9f4dab3f210d90e4752dd8c982a7ee8d
kabatan <bakatan.kabatan@gmail.com>
2026-06-15 09:01:03 +0900

    Pass OrderCase Full2D artifact threshold

---
+++ runs/geometry_full2d_v0_4_2/proof_artifact_batch/full2d-curated-2414/source_problem.lean
@@ -0,0 +1,8 @@
+import MathAutoResearch.GeometryFull2D.Extraction
+
+open MathAutoResearch.GeometryFull2D
+
+theorem full2d_curated_2414 (A B C : Point) (h : between A B C) : collinear A B C := by
+  -- MARP_PROOF_REGION_START:full2d_curated_2414
+  sorry
+  -- MARP_PROOF_REGION_END:full2d_curated_2414
